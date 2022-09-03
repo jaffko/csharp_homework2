813d41438ec8ac00dfe68eab41cbfc9d12d1cba4
Ildar <steel14@yandex.ru>
2022-09-03 21:08:36 +0500

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности число и удаляет вторую цифру слева направо этого числа.
+
+void KillSecondNumber(int num)
+{
+    int numCount = num;
+    int numLen = 1;
+    while (numCount / 10 != 0)
+    {
+        numCount = numCount / 10;
+        numLen++;
+    }
+    int tenPow = 1;
+    while (numLen > 1)
+    {
+        tenPow = tenPow * 10;
+        numLen--;
+    }
+    Console.WriteLine(num / tenPow * tenPow / 10 + num % (tenPow / 10));
+}
+
+Console.Write("Введите целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+KillSecondNumber(x);
