466cba9f83a510b73ec0889e107af049925144d4
Ildar <steel14@yandex.ru>
2022-09-03 21:21:54 +0500

    added task5

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿string ChooseWord(int n)
+{
+    if (n >= 5 & n <= 20) return "программистов";
+    else if (n % 10 == 1) return "програмист";
+    else if (n % 10 >= 2 & n % 10 <= 4) return "программиста";
+    return "программистов";
+}
+
+Console.Write("Введите количество программистов в комнате: ");
+int progNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"В комнате {progNumber} {ChooseWord(progNumber)}");
