4b3a7b2cd77a4a2c22b2a77fb07981f261f3e30f
Ildar <steel14@yandex.ru>
2022-09-03 20:14:41 +0500

    added 1st task

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondNumber (int num)
+{
+    Console.WriteLine($"Число {num}, вторая цифра числа {num / 10 % 10}");
+}
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+SecondNumber(x);
